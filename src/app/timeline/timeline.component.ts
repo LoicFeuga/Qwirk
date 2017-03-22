95d335036b8a9856baab5e0d3f47eb6f78c7042d
Loïc Feuga <loic.feuga@gmail.com>
2017-03-22 15:01:11 +0100

    style du bas de la timeline pour call et video

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -10,10 +10,14 @@ export class TimelineComponent implements OnInit {
 
   messages: Object[] = [];
   text: string;
+  //0 = message;
+  //1 = audio;
+  //2 = video
+  modeTimeline = 0;
 
   constructor() {
 
-    this.messages.push({author:"loic",content:"loremkojaoj oijoiaj ioj"});
+    this.messages.push({ author: "loic", content: "loremkojaoj oijoiaj ioj" });
     setTimeout(function () {
       let objDiv = document.querySelector("#app-timeline");
       objDiv.scrollTop = objDiv.scrollHeight;
@@ -23,12 +27,28 @@ export class TimelineComponent implements OnInit {
   scrollBot() {
   }
 
+
+
+  isChatSelected() {
+    return this.modeTimeline == 0;
+  }
+  isCallSelected(){
+    return this.modeTimeline == 1;
+  }
+
+  setMode(mode : number){
+    this.modeTimeline = mode;
+  }
+  isVideoSelected(){
+    return this.modeTimeline == 2;
+  }
+
   add() {
     this.messages.push({ author: "loic", content: this.text });
     setTimeout(function () {
       let objDiv = document.querySelector("#app-timeline");
       objDiv.scrollTop = objDiv.scrollHeight;
-    },1);
+    }, 1);
     this.text = "";
   }
   ngOnInit() {
