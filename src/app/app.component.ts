6dc45f99498fe044b82d7a7cbf4eba66d107b9d7
Loïc Feuga <loic.feuga@gmail.com>
2017-06-07 10:00:00 +0200

    Connexion = statut En ligne, Deconnexion = statut Hors Ligne

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,50 @@
-import { Component } from '@angular/core';  
+import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { VMenuComponent } from './vmenu/vmenu.component';
+import { UsersService } from './users.service';
+import { AuthenticationService } from './authentication.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  appName : string = "Qwirk";
-  isLog : number = 0;
-  
-  
-  onLogged(recup : boolean){    
+  appName: string = "Qwirk";
+  isLog: number = 0;
+
+  constructor(private usersService: UsersService, private auth: AuthenticationService) {
+
+  }
+
+  onLogged(recup: boolean) {
     document.querySelector('.root')['style'].background = "#bbdefb";
     this.isLog = recup == true ? 1 : 0;
   }
 
-  goFiltre(event : any){
-    if(event.code == "KeyF" && event.ctrlKey == true ){
+  goFiltre(event: any) {
+    if (event.code == "KeyF" && event.ctrlKey == true) {
       document.getElementById('search').focus();
       return false;
     }
-    
+
   }
 
-  onDeco(recup : boolean){
-    this.isLog = 0;  
-    localStorage.setItem('user','');
+  onDeco(recup: boolean) {
+
+    let that = this;
+    this.usersService.getUser(this.auth.getUserID(), function (data) {
+      data.statut = "Hors Ligne";
+      that.usersService.updateUser(data, function (data) {
+        
+      });
+    });
+
+    this.isLog = 0;
+    localStorage.setItem('user', '');
     document.querySelector('.root')['style'].background = "#FFFFFF";
-    
+
+
     window.location.reload();
 
   }
